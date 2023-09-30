d0b9b15104fba3defa276dccf5f3611fadb1c660
DebsonCoelho <debsoncoelh@gmail.com>
2023-09-30 18:34:17 -0300

    first commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,18 @@
+/* eslint-disable react/style-prop-object */
+import { GluestackUIProvider, config } from '@gluestack-ui/themed';
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import Routes from 'routes/index';
 
-export default function App() {
+const App: React.FC = () => {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <GluestackUIProvider config={config.theme}>
+        <Routes />
+        <StatusBar style="auto" />
+      </GluestackUIProvider>
+    </NavigationContainer>
   );
-}
+};
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
